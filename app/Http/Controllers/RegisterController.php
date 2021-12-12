98466542dc1ba4ab018e08832a3a78fdb6f7c6a8
nan74k <nanta.pslove@gmail.com>
2021-12-12 17:32:20 +0700

    add login and register features

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -11,12 +13,22 @@ class RegisterController extends Controller
         $request->validate([
             'firstname' => ['required', 'not_regex:/[@$!%*#?&]/', 'not_regex:/[0-9]/','max:255'],
             'lastname' => ['nullable','not_regex:/[@$!%*#?&]/', 'not_regex:/[0-9]/', 'max:255'],
-            'email' => 'required|unique:users|email:dns',
+            'email_register' => 'required|unique:users|email:dns',
             'password_register' => ['required', 'min:8', 'regex:/[a-z]/', 'regex:/[A-Z]/', 'regex:/[0-9]/'],
             'confirmpassword' => ['required', 'min:8', 'same:password_register'],
             'phone_number' => ['required', 'unique:users', 'not_regex:/[@$!%*#?&]/', 'not_regex:/[A-Z]/', 
             'not_regex:/[a-z]/', 'min:9','max:12']
         ]);
+
+        $user = new User;
+
+        $user->firstname = $request->firstname;
+        $user->lastname = $request->lastname;
+        $user->email = $request->email_register;
+        $user->password = Hash::make($request->password_register);
+        $user->phone_number = $request->phone_number;
+
+        $user->save();
 
         return redirect('/');
     }
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,23 +1,62 @@
 <div class="min-w-full flex justify-between container px-20 my-8">
         <div class="flex">
-            <div class="flex items-center space-x-4">
-                <img src="img/single-bed.png" alt="icon" style="width: 30px; height: 30px">
-                <span class="text-gray-500 text-2xl font-semibold">Comfy</span>
-            </div>
+            <a href="/">
+                <div class="flex items-center space-x-4 cursor-pointer" id="icon">
+                    <img src="img/single-bed.png" alt="icon" style="width: 30px; height: 30px">
+                    <span class="text-gray-500 text-2xl font-semibold">Comfy</span>
+                </div>
+            </a>
         </div>
         <div class="flex mt-2">
             <div class="flex space-x-14">
                 <a href="#" class="font-semibold text-gray-400 hover:text-gray-700">Discover</a>
                 <a href="#" class="font-semibold text-gray-400 hover:text-gray-700">Special Offer</a>
                 <a href="#" class="font-semibold text-gray-400 hover:text-gray-700">About Us</a>
-                <button type="submit" class="w-24 h-8 -mt-1 font-semibold text-indigo-500 ring-2 rounded-lg hover:bg-blue-600 hover:text-white" id="login-btn">
-                    Login
-                    {{-- @if ($title == 'Comfy')
-                        <a href="/login">Login</a>
-                    @elseif($title == 'Comfy | Login')
-                        <a href="/register">Register</a>
-                    @endif --}}
-                </button>
+                @if (auth()->user())
+                <div class="block h-7" id="account-login">
+                    <div class="flex items-center space-x-2 cursor-pointer hover:text-gray-700">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clip-rule="evenodd" />
+                        </svg>
+                        <span class="font-semibold text-gray-400">Ananta</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mt-1 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div class="w-32 h-auto bg-white absolute hidden" id="account">
+                        <div class="w-full flex items-center border-b cursor-pointer px-2 space-x-2 pt-2 pb-1 hover:bg-blue-50">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+                            </svg>
+                            <span>Profil</span>
+                        </div>
+                        <div class="w-full flex items-center cursor-pointer px-2 space-x-2 pt-1 pb-2 hover:bg-blue-50">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                                <path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd" />
+                            </svg>
+                            <form action="/logout" method="post">
+                                @csrf
+                                <button type="submit">Logout</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                @else
+                    <button type="submit" class="w-24 h-8 -mt-1 font-semibold text-indigo-500 ring-2 rounded-lg hover:bg-blue-600 hover:text-white" id="login-btn">
+                        Login
+                    </button>
+                @endif
             </div>
         </div>
 </div>
+
+{{-- hover mouse account --}}
+<script>
+    document.getElementById('account-login').addEventListener('mouseover', function(){
+        document.getElementById('account').style.display = 'block'
+    })
+
+    document.getElementById('account-login').addEventListener('mouseout', function(){
+        document.getElementById('account').style.display = 'none'
+    })
+</script>
